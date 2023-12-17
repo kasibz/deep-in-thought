09a4687d4d9d492b4c786208f66814aa1dbaec9b
yuhojong91 <hojong.yu@fiserv.com>
2023-12-17 01:39:16 -0500

    Created dialog/popup for adding property in owner property component

--- frontend/deep-in-thought-frontend/src/components/OwnerPropertyComponent.jsx
+++ frontend/deep-in-thought-frontend/src/components/OwnerPropertyComponent.jsx
@@ -1,11 +1,51 @@
-import { Container, List, ListItem, ListItemText, Typography, Button, Divider } from '@mui/material';
+import { Container, List, ListItemButton, ListItemText, Typography, Button, Divider, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, TextField, ListItem } from '@mui/material';
+import { Fragment, useState } from 'react';
 
-import { Fragment } from 'react';
 const myProperties = [
     { id: 1, name: 'Lakeside Condo', type: 'Condo', address: '123 Lakeview St' },
     { id: 2, name: 'Downtown Loft', type: 'Loft', address: '456 Citycenter Ave' },
 ];
+
 const OwnerPropertyComponent = () => {
+    const [selectedProperty, setSelectedProperty] = useState(null);
+    
+    // open and close state variable for dialog
+    const [open, setOpen] = useState(false);
+
+    // new property state variable
+    const [newProperty, setNewProperty] = useState({
+        name: '',
+        type: '',
+        address: ''
+    });
+
+    const onClickOpenDialog = () => {
+        // Open the dialog
+        setOpen(true);
+    };
+
+    const onClickCloseDialog = () => {
+        // Close the dialog
+        setOpen(false);
+    };
+
+    const onClickProperty = (property) => {
+        console.log(property);
+    };
+
+    const onChangeAddPropertyTextField = (e) => {
+        setNewProperty({ ...newProperty, [e.target.name]: e.target.value });
+    };
+
+    const onSubmitAddProproperty = () => {
+
+        console.log(newProperty);
+        // Reset the form
+        setNewProperty({ name: '', type: '', address: '' });
+        // Close the dialog
+        setOpen(false);
+    };
+
     return (
         <Container>
             <Typography variant="h4" sx={{ mt: 4, mb: 2 }}>
@@ -14,28 +54,62 @@ const OwnerPropertyComponent = () => {
             <List>
                 {myProperties.map((property, index) => (
                     <Fragment key={property.id}>
-                        <ListItem>
+                        <ListItemButton onClick={() => onClickProperty(property)}>
                             <ListItemText
                                 primary={property.name}
                                 secondary={`Type: ${property.type} - Address: ${property.address}`}
                             />
-                        </ListItem>
+                        </ListItemButton>
                         {index !== myProperties.length - 1 && <Divider />}
                     </Fragment>
                 ))}
                 <ListItem>
-                    <Button
-                        variant="outlined"
-                        // startIcon={<AddIcon />}
-                        fullWidth
-                        // onClick={onAddProperty}
-                    >
+                    <Button onClick={onClickOpenDialog} variant="outlined" fullWidth>
                         Add Property
                     </Button>
                 </ListItem>
             </List>
+
+            {/* Add Property Dialog */}
+            <Dialog open={open} onClose={onClickCloseDialog}>
+                <DialogTitle>Add New Property</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        name="name"
+                        label="Name"
+                        fullWidth
+                        variant="standard"
+                        value={newProperty.name}
+                        onChange={onChangeAddPropertyTextField}
+                    />
+                    <TextField
+                        margin="dense"
+                        name="type"
+                        label="Type"
+                        fullWidth
+                        variant="standard"
+                        value={newProperty.type}
+                        onChange={onChangeAddPropertyTextField}
+                    />
+                    <TextField
+                        margin="dense"
+                        name="address"
+                        label="Address"
+                        fullWidth
+                        variant="standard"
+                        value={newProperty.address}
+                        onChange={onChangeAddPropertyTextField}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={onClickCloseDialog}>Cancel</Button>
+                    <Button onClick={onSubmitAddProproperty}>Submit</Button>
+                </DialogActions>
+            </Dialog>
         </Container>
-    )
+    );
 }
 
-export default OwnerPropertyComponent
+export default OwnerPropertyComponent;
