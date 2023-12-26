08c8f6d38c37919c68fd3c0f1a856b17011f79ec
yuhojong91 <hojong.yu@fiserv.com>
2023-12-26 11:41:09 -0500

    Created basic financial statement page for Owner

--- frontend/deep-in-thought-frontend/src/pages/FinancialStatementsPage.jsx
+++ frontend/deep-in-thought-frontend/src/pages/FinancialStatementsPage.jsx
@@ -1,10 +1,39 @@
+import { useEffect, useState } from "react"
 import FinancialStatementsComponent from "../components/user/FinancialStatementsComponent"
+import paymentService from "../utilities/paymentService"
+import { UserContext } from "../context/UserContext";
+import { Box, CircularProgress } from "@mui/material";
 
 const FinancialStatementsPage = () => {
+    const [propertiesData, setPropertiesData] = useState()
+
+    const { user } = UserContext();
+    const onwerId = user[0].ownerId
+
+    useEffect(() => {
+        const getAllPaymentHistoryForOwner = async () => {
+            try {
+                const response = await paymentService.getAllPaymentsByOwnerId(onwerId)
+                if (response.status === 200) {
+                    setPropertiesData(response.data)
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getAllPaymentHistoryForOwner()
+    }, [])
+
     return (
-        <>
-            <FinancialStatementsComponent/>
-        </>
+        <div className="container">
+            {propertiesData ? (
+                <FinancialStatementsComponent propertiesData={propertiesData} />
+            ) : (
+                <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
+                    <CircularProgress />
+                </Box>
+            )}
+        </div>
     )
 }
 
