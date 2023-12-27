de261d46ad81b644349e8a7759862bf5f1f5bd60
kasib <kasibzxc@gmail.com>
2023-12-27 14:03:12 -0500

    add properties with dropdown

--- frontend/deep-in-thought-frontend/src/components/OwnerPropertiesListComponent.jsx
+++ frontend/deep-in-thought-frontend/src/components/OwnerPropertiesListComponent.jsx
@@ -1,164 +1,228 @@
-import { Container, List, ListItemButton, ListItemText, Typography, Button, Divider, Dialog, DialogTitle, DialogContent, DialogActions, TextField, ListItem, Box, CircularProgress } from '@mui/material';
-import { Fragment, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router';
-import { useMyPropertyContext } from './../context/PropertyContext';
-import AddPropertyDialog from './dialogs/AddPropertyDialog';
-import propertyService from '../utilities/propertyService';
-import { UserContext } from './../context/UserContext';
-import SuccessSnackBar from './snackbar/SuccessSnackBar';
+import {
+  Container,
+  List,
+  ListItemButton,
+  ListItemText,
+  Typography,
+  Button,
+  Divider,
+  ListItem,
+  Box,
+  CircularProgress,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { useMyPropertyContext } from "./../context/PropertyContext";
+import AddPropertyDialog from "./dialogs/AddPropertyDialog";
+import propertyService from "../utilities/propertyService";
+import { UserContext } from "./../context/UserContext";
+import SuccessSnackBar from "./snackbar/SuccessSnackBar";
 
 const OwnerPropertyComponent = () => {
-    // calling user context
-    const { user } = UserContext();
+  // calling user context
+  const { user } = UserContext();
 
-    // navigate hook to allow navigate to different routes
-    const navigate = useNavigate();
+  // navigate hook to allow navigate to different routes
+  const navigate = useNavigate();
 
-    // open and close state variable for dialog
-    const [open, setOpen] = useState(false);
+  // open and close state variable for dialog
+  const [open, setOpen] = useState(false);
 
-    // new property state variable
-    const [newProperty, setNewProperty] = useState({
-        name: '',
-        type: '',
-        streetAddress: '',
-        city: '',
-        state: '',
-        zipcode: '',
-        ownerId: user[0].ownerId,
-    });
+  // new property state variable
+  const [newProperty, setNewProperty] = useState({
+    name: "",
+    type: "",
+    streetAddress: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    ownerId: user[0].ownerId,
+  });
 
-    // calling property context state variables
-    const { ownerProperties, addOwnerProperty } = useMyPropertyContext();
-    //loading variable 
-    const [isLoading, setIsLoading] = useState(true);
+  // calling property context state variables
+  const { ownerProperties, addOwnerProperty } = useMyPropertyContext();
+  //loading variable
+  const [isLoading, setIsLoading] = useState(true);
 
-    // get list of owner's list of properties and persist data
-    useEffect(() => {
-        const getOwnerProperties = async () => {
-            try {
-                const response = await propertyService.getAllPropertiesByIdForOwner(user[0].ownerId)
-                // console.log(response)
-                if (response.status === 200) {
-                    addOwnerProperty(response.data)
-                }
-                setIsLoading(false); // Set loading to false once data has bee fetched
-            } catch (error) {
-                console.log(error)
-                setIsLoading(false); // Set loading to false once data has bee fetched
-            }
-        }
-        getOwnerProperties()
-    }, [open]) // whenever there is a change in this variable, this useEffect will be triggered.
-
-    const onClickOpenDialog = () => {
-        // Open the dialog
-        setOpen(true);
-    };
-
-    const onClickCloseDialog = () => {
-        // Close the dialog
-        setOpen(false);
-    };
-
-    const onClickProperty = (property) => {
-        console.log(property);
-        navigate(`/property/${property.id}`)
-    };
-
-    const onChangeAddPropertyTextField = (e) => {
-        setNewProperty({ ...newProperty, [e.target.name]: e.target.value });
-    };
-
-    //snack bar state variables
-    const [snackbarOpen, setSnackbarOpen] = useState(false);
-    const [snackbarMessage, setSnackbarMessage] = useState('');
-
-    //snack bar on close function
-    const handleCloseSnackbar = () => {
-        setSnackbarOpen(false);
-    };
-
-    const onSubmitAddProproperty = async () => {
-        console.log(newProperty);
-        try {
-            const addPropertyResponse = await propertyService.addProperty(newProperty);
-            console.log(addPropertyResponse)
-            if (addPropertyResponse.status === 201) {
-                //snack bar message
-                setSnackbarMessage('Property added successfully!');
-                //set true to open snack bar
-                setSnackbarOpen(true);
-                // close dialog
-                setOpen(false);
-            } else {
-                alert('something is wrong. This need to be changed')
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    };
-    // display none when loading variable is true
-    if (isLoading) {
-        return (
-            <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
-                <CircularProgress />
-            </Box>
+  // get list of owner's list of properties and persist data
+  useEffect(() => {
+    const getOwnerProperties = async () => {
+      try {
+        const response = await propertyService.getAllPropertiesByIdForOwner(
+          user[0].ownerId
         );
+        // console.log(response)
+        if (response.status === 200) {
+          addOwnerProperty(response.data);
+        }
+        setIsLoading(false); // Set loading to false once data has bee fetched
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false); // Set loading to false once data has bee fetched
+      }
+    };
+    getOwnerProperties();
+  }, [open]); // whenever there is a change in this variable, this useEffect will be triggered.
+
+  const onClickOpenDialog = () => {
+    // Open the dialog
+    setOpen(true);
+  };
+
+  const onClickCloseDialog = () => {
+    // Close the dialog
+    setOpen(false);
+  };
+
+  const onClickProperty = (property) => {
+    console.log(property);
+    navigate(`/property/${property.id}`);
+  };
+
+  const onChangeAddPropertyTextField = (e) => {
+    setNewProperty({ ...newProperty, [e.target.name]: e.target.value });
+  };
+
+  //snack bar state variables
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+
+  //snack bar on close function
+  const handleCloseSnackbar = () => {
+    setSnackbarOpen(false);
+  };
+
+  const onSubmitAddProproperty = async () => {
+    console.log(newProperty);
+    try {
+      const addPropertyResponse = await propertyService.addProperty(
+        newProperty
+      );
+      console.log(addPropertyResponse);
+      if (addPropertyResponse.status === 201) {
+        //snack bar message
+        setSnackbarMessage("Property added successfully!");
+        //set true to open snack bar
+        setSnackbarOpen(true);
+        // close dialog
+        setOpen(false);
+      } else {
+        alert("something is wrong. This need to be changed");
+      }
+    } catch (error) {
+      console.log(error);
     }
-
+  };
+  // display none when loading variable is true
+  if (isLoading) {
     return (
-        <Container className='container' sx={{ width:'600px', minWidth:'100px' }}>
-            <List className='general-box' sx={{ maxHeight: '600px', overflowY: 'auto' }}>
-                <ListItem>
-                    <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', width: '100%', gap: 6 }}>
-                        <Typography variant="h4">
-                            My properties
-                        </Typography>
-                        <Button size='small' onClick={onClickOpenDialog} variant="contained">
-                            Add Property
-                        </Button>
-                    </Box>
-                </ListItem>
-                {ownerProperties && ownerProperties.length > 0 ? ownerProperties.map((property, index) => (
-                    <Box key={property.id}>
-                        <ListItemButton onClick={() => onClickProperty(property)}>
-                            <ListItemText sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center' }}
-                                primary={
-                                    <Typography variant="h6">
-                                        {`Property : ${property.name}`}
-                                    </Typography>}
-                                secondary={
-                                    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center' }}>
-                                        <Typography component="span" variant="body1" color="textPrimary">
-                                            Type: {property.type}
-                                        </Typography>
-                                        <Typography component="span" variant="body1" color="textPrimary">
-                                            Address: {property.streetAddress} {property.city} {property.state} {property.zipcode}
-                                        </Typography>
-                                    </Box>}
-                            />
-                        </ListItemButton>
-                        {index !== ownerProperties.length - 1 && <Divider />}
-                    </Box>
-                )) : <div>You currently have no properties listed. Click here to add your first property</div>}
-            </List>
-
-            {/* Add Property Dialog */}
-            <AddPropertyDialog
-                open={open}
-                onClose={onClickCloseDialog}
-                newProperty={newProperty}
-                onChange={onChangeAddPropertyTextField}
-                onSubmit={onSubmitAddProproperty}
-            />
-            <SuccessSnackBar
-                open={snackbarOpen}
-                message={snackbarMessage}
-                handleClose={handleCloseSnackbar}
-            />
-        </Container>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+      >
+        <CircularProgress />
+      </Box>
     );
-}
+  }
+
+  return (
+    <Container className="container" sx={{ width: "600px", minWidth: "100px" }}>
+      <List
+        className="general-box"
+        sx={{ maxHeight: "600px", overflowY: "auto" }}
+      >
+        <ListItem>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              width: "100%",
+              gap: 6,
+            }}
+          >
+            <Typography variant="h4">My properties</Typography>
+            <Button
+              size="small"
+              onClick={onClickOpenDialog}
+              variant="contained"
+            >
+              Add Property
+            </Button>
+          </Box>
+        </ListItem>
+        {ownerProperties && ownerProperties.length > 0 ? (
+          ownerProperties.map((property, index) => (
+            <Box key={property.id}>
+              <ListItemButton onClick={() => onClickProperty(property)}>
+                <ListItemText
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    textAlign: "center",
+                  }}
+                  primary={
+                    <Typography variant="h6">
+                      {`Property : ${property.name}`}
+                    </Typography>
+                  }
+                  secondary={
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        textAlign: "center",
+                      }}
+                    >
+                      <Typography
+                        component="span"
+                        variant="body1"
+                        color="textPrimary"
+                      >
+                        Type: {property.type}
+                      </Typography>
+                      <Typography
+                        component="span"
+                        variant="body1"
+                        color="textPrimary"
+                      >
+                        Address: {property.streetAddress} {property.city}{" "}
+                        {property.state} {property.zipcode}
+                      </Typography>
+                    </Box>
+                  }
+                />
+              </ListItemButton>
+              {index !== ownerProperties.length - 1 && <Divider />}
+            </Box>
+          ))
+        ) : (
+          <div>
+            You currently have no properties listed. Click here to add your
+            first property
+          </div>
+        )}
+      </List>
+
+      {/* Add Property Dialog */}
+      <AddPropertyDialog
+        open={open}
+        onClose={onClickCloseDialog}
+        newProperty={newProperty}
+        onChange={onChangeAddPropertyTextField}
+        onSubmit={onSubmitAddProproperty}
+      />
+      <SuccessSnackBar
+        open={snackbarOpen}
+        message={snackbarMessage}
+        handleClose={handleCloseSnackbar}
+      />
+    </Container>
+  );
+};
 
 export default OwnerPropertyComponent;
--- frontend/deep-in-thought-frontend/src/components/dialogs/AddPropertyDialog.jsx
+++ frontend/deep-in-thought-frontend/src/components/dialogs/AddPropertyDialog.jsx
@@ -1,73 +1,101 @@
-import { Button, Dialog, DialogTitle, DialogContent, DialogActions, TextField } from '@mui/material';
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+} from "@mui/material";
+import States from "../../data/states.json";
 
-const AddPropertyDialog = ({ open, onClose, newProperty, onChange, onSubmit }) => {
-    return (
-        <Dialog open={open} onClose={onClose}>
-            <DialogTitle>Add New Property</DialogTitle>
-            <DialogContent>
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    name="name"
-                    label="Name"
-                    fullWidth
-                    variant="standard"
-                    value={newProperty.name}
-                    onChange={onChange}
-                />
-                <TextField
-                    margin="dense"
-                    name="type"
-                    label="Type"
-                    fullWidth
-                    variant="standard"
-                    value={newProperty.type}
-                    onChange={onChange}
-                />
-                <TextField
-                    margin="dense"
-                    name="streetAddress"
-                    label="Street"
-                    fullWidth
-                    variant="standard"
-                    value={newProperty.streetAddress}
-                    onChange={onChange}
-                />
-                <TextField
-                    margin="dense"
-                    name="city"
-                    label="City"
-                    fullWidth
-                    variant="standard"
-                    value={newProperty.city}
-                    onChange={onChange}
-                />
-                <TextField
-                    margin="dense"
-                    name="state"
-                    label="State"
-                    fullWidth
-                    variant="standard"
-                    value={newProperty.state}
-                    onChange={onChange}
-                />
-                <TextField
-                    margin="dense"
-                    name="zipcode"
-                    label="Zipcode"
-                    fullWidth
-                    variant="standard"
-                    value={newProperty.zipcode}
-                    onChange={onChange}
-                />
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={onClose}>Cancel</Button>
-                <Button onClick={onSubmit}>Submit</Button>
-            </DialogActions>
-        </Dialog>
-    )
-}
+const AddPropertyDialog = ({
+  open,
+  onClose,
+  newProperty,
+  onChange,
+  onSubmit,
+}) => {
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>Add New Property</DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          margin="dense"
+          name="name"
+          label="Name"
+          fullWidth
+          variant="standard"
+          value={newProperty.name}
+          onChange={onChange}
+        />
+        <TextField
+          margin="dense"
+          name="type"
+          label="Type"
+          fullWidth
+          variant="standard"
+          value={newProperty.type}
+          onChange={onChange}
+        />
+        <TextField
+          margin="dense"
+          name="streetAddress"
+          label="Street"
+          fullWidth
+          variant="standard"
+          value={newProperty.streetAddress}
+          onChange={onChange}
+        />
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <TextField
+            margin="dense"
+            name="city"
+            label="City"
+            variant="standard"
+            value={newProperty.city}
+            onChange={onChange}
+          />
+          <FormControl sx={{ m: 1, minWidth: 80, marginRight: 10 }}>
+            <InputLabel id="demo-simple-select-helper-label">State</InputLabel>
+            <Select
+              labelId="demo-simple-select-helper-label"
+              id="demo-simple-select-helper"
+              value={newProperty.state}
+              name="state"
+              label="States"
+              onChange={onChange}
+            >
+              {States.map((state, idx) => {
+                return (
+                  <MenuItem key={idx} value={state}>
+                    {state}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+        </div>
+        <TextField
+          margin="dense"
+          name="zipcode"
+          label="Zipcode"
+          fullWidth
+          variant="standard"
+          value={newProperty.zipcode}
+          onChange={onChange}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Cancel</Button>
+        <Button onClick={onSubmit}>Submit</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
 
-
-export default AddPropertyDialog
+export default AddPropertyDialog;
